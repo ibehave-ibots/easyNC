db7f37cd5c3b4623d61d254f875dc48621b179d2
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-11-20 18:44:35 +0100

    added Write.write_From_struct

--- +netcdf/Writer.m
+++ +netcdf/Writer.m
@@ -22,5 +22,16 @@ classdef Writer
         function [] = write_attributes(obj, varargin)
             createNcAttributes(obj.filename, varargin{:});
         end
+
+        function [] = write_from_struct(obj, dset, varName, dimensions, varargin)
+            for name = dimensions
+                name = name{1};
+                data = dset.(name);
+                size(data)
+                createNcDimension(obj.filename, name, data);
+            end
+            size(dset.(varName))
+            createNcVariable(obj.filename, varName, dset.(varName), dimensions, varargin{:})
+        end
     end
 end
